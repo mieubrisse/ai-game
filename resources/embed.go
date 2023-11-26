bb825c8330824d06d3178090bcbd6e7ea6335dfd
mieubrisse <mieubrisse@gmail.com>
2023-11-25 21:58:41 -0300

    Init commit

---
+++ resources/embed.go
@@ -0,0 +1,14 @@
+package resources
+
+import _ "embed"
+
+var (
+	//go:embed mage.png
+	MagePNG []byte
+
+	//go:embed mage2.png
+	Mage2PNG []byte
+
+	//go:embed magic-ball.png
+	MagicBallPNG []byte
+)
